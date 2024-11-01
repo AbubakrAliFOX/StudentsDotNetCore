18839f9ada090269dd3d31a04b19a5de2caffc7b
Abubakr <thefox5000@gmail.com>
2024-11-01 20:28:54 +0300

    Create first endpoint

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsAPI.DataSimulation;
+using StudentsAPI.Model;
+
+namespace StudentsAPI.Controllers
+{
+    [Route("api/Students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetAllStudents ()
+        {
+            return Ok(StudentsDataSimulation.StudentsList);
+        }
+    }
+}
---
+++ DataSimulation/StudentsDataSimulation.cs
@@ -0,0 +1,17 @@
+﻿using StudentsAPI.Model;
+
+
+namespace StudentsAPI.DataSimulation
+{
+    public class StudentsDataSimulation
+    {
+        public static readonly List<Student> StudentsList = new List<Student>
+        {
+            new Student {ID =  1, Name = "Khaled", Age = 22, Grade = 90},
+            new Student {ID =  2, Name = "Ahmad", Age = 25, Grade = 23},
+            new Student {ID =  3, Name = "Mohammad", Age = 26, Grade = 62},
+            new Student {ID =  4, Name = "Abdullah", Age = 23, Grade = 77},
+            new Student {ID =  5, Name = "Malsef", Age = 21, Grade = 60}
+        };
+    }
+}
